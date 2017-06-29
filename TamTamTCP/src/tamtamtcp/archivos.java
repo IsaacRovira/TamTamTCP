d841c4ecab4d6806fb85ae110285f407d39b9707
Isaac Rovira <rovira.isaac@gmail.com>
2017-06-29 07:55:40 +0200

    Some addtion to the class archivos

--- TamTamTCP/src/tamtamtcp/archivos.java
+++ TamTamTCP/src/tamtamtcp/archivos.java
@@ -12,22 +12,43 @@ import java.util.*;
  *
  * @author Isaac
  */
+
+/*
+File[] files = new File("/path/to/the/directory").listFiles(new FilenameFilter()
+{ @Override public boolean accept(File dir, String name)
+{ return name.endsWith(".xml"); } });
+*/
 public class archivos {
     String nombreArchivo;
-    String rutaArchivo;
-    Directory directorio;
+    String rutaArchivos;
+    File directorio;
     File file; 
     Scanner lector;
     PrintWriter escritor;
+    File[] listaDeFicheros;
     
-    private ArrayList abrirfichero(){
+    archivos(){
+        this.nombreArchivo = null;
+        this.rutaArchivos = null;
+        this.directorio = null;
+        this.file = null;
+        this.lector = null;
+        this. escritor = null;
+        this.listaDeFicheros = null;
+    }
+    
+    archivos(String nombreArchivo, String rutaArchivos){
+        this.nombreArchivo = nombreArchivo;
+        this.rutaArchivos = rutaArchivos;
+        this.directorio = new File(rutaArchivos);        
+    }
+    
+    public ArrayList abrirfichero(File file){
         ArrayList datos = new ArrayList();
-        try{
-            file = new File(nombreArchivo);
+        try{            
             lector = new Scanner(file);
             while(lector.hasNextLine()){
-                datos.add(lector.nextLine());
-                
+                datos.add(lector.nextLine());                
             }
         }catch(Exception e){
             System.err.println(e.getMessage());
@@ -38,5 +59,15 @@ public class archivos {
         return datos;
     }
     
+    private File[] getFiles(){        
+        directorio.listFiles();
+    }    
     
+    private int numOfFiles(){
+        int num = 0;
+        for(File f: listaDeFicheros){
+            if(f.isFile()) num++;
+        }
+        return num;
+    }    
 }
